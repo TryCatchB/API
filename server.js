1a0052010f4681975ef1f4c9e9f93d1db7ede76b
TryCatchB <pozdrakovzena@gmail.com>
2024-10-25 18:30:30 +0300

    Created Available Categories page

--- server.js
+++ server.js
@@ -25,6 +25,35 @@ getData(app, "/tv-shows", tvShows);
 
 getData(app, "/newest-films", newestFilms);
 
+app.get("/", (req, res) => {
+  res.send(`
+    <div>
+      <h1>Welcome to Custom API</h1>
+      <p style="font-size: 25px">Available Categories:</p>
+      <ul style="font-size: 20px">
+        <li>
+          <a href="/projects">projects</a>
+        </li>
+        <li>
+          <a href="/projects/:id">projects:id</a>
+        </li>
+        <li>
+          <a href="/popular-films">popular-films</a>
+        </li>
+        <li>
+          <a href="/films">films</a>
+        </li>
+        <li>
+          <a href="/tv-shows">tv-shows</a>
+        </li>
+        <li>
+          <a href="/newest-films">newest-films</a>
+        </li>
+        </ul>
+    </div>
+  `);
+});
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
